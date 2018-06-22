6dea157c5296ac2332aea165c9d266a008df7843
Kayrhen <karen.ding@yahoo.com>
2018-06-22 15:25:17 -0500

    router added and font-awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,24 @@ import { MenuComponent } from './menu/menu.component';
 import {CommonModule} from '@angular/common';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import {DishService} from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import {AppRoutingModule} from './app-routing/app-routing.module';
 //import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
@@ -24,6 +35,7 @@ import {DishService} from './services/dish.service';
     FormsModule,
     HttpModule,
     //MaterialModule,
+    AppRoutingModule,
     CommonModule,
     MatGridListModule,
     MatListModule,
